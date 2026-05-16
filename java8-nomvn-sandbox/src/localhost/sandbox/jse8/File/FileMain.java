744fbf53132403da67608bbed630fd6ae52b9b10
Alberto Romero <alberto.romero.eng@gmail.com>
2026-05-16 18:29:06 -0400

    [java] java8-nomvn-sandbox, enhance File dirCurrentAndRoot tests

--- java8-nomvn-sandbox/src/localhost/sandbox/jse8/File/FileMain.java
+++ java8-nomvn-sandbox/src/localhost/sandbox/jse8/File/FileMain.java
@@ -6,6 +6,6 @@ public class FileMain {
 		System.out.println("Hello from FileMain!");
 		// Test00FileReader.simpleReadfile();
 		// Test01FileWriter.simpleWriteToFile();
-		Test02File.test00FileCurrentDir();
+		Test02File.test00FileDirCurrentAndRoot();
 	}
 }
--- java8-nomvn-sandbox/src/localhost/sandbox/jse8/File/Test02File.java
+++ java8-nomvn-sandbox/src/localhost/sandbox/jse8/File/Test02File.java
@@ -11,68 +11,64 @@ import java.io.File;
  */
 public class Test02File {
 
-	public static void test00FileCurrentDir() {
+	public static void test00FileDirCurrentAndRoot() {
 		// String relPath = "tmp/numbers.txt";
 
 		// file 1
 		System.out.println("--- file1 ---");
-		String relPath1 = "";
-		File file1 = new File(relPath1);
-		boolean file1_IsAbs = file1.isAbsolute();
-		boolean file1_Exists = file1.exists();
-		boolean file1_IsDir = file1.isDirectory();
-		boolean file1_IsFile = file1.isFile();
-		String file1_Name = file1.getName();
-		String file1_AbsPath = file1.getAbsolutePath();
-		String file1_Path = file1.getPath();
-		System.out.println("file1_Src:     '" + relPath1 + "'");
-		System.out.println("file1_IsAbs:   '" + file1_IsAbs + "'");
-		System.out.println("file1_Exists:  '" + file1_Exists + "'");
-		System.out.println("file1_IsDir:   '" + file1_IsDir + "'");
-		System.out.println("file1_IsFile:  '" + file1_IsFile + "'");
-		System.out.println("file1_Name:    '" + file1_Name + "'");
-		System.out.println("file1_Path:    '" + file1_Path + "'");
-		System.out.println("file1_AbsPath: '" + file1_AbsPath + "'");
+		String srcPath1 = "";
+		File file1 = new File(srcPath1);
+		System.out.println("srcPath1:      '" + srcPath1 + "'");
+		printFileProperties(file1);
 
 		// file 2
 		System.out.println("--- file2 ---");
-		String absPath2 = file1_AbsPath;
-		File file2 = new File(absPath2);
-		boolean file2_IsAbs = file2.isAbsolute();
-		boolean file2_Exists = file2.exists();
-		boolean file2_IsDir = file2.isDirectory();
-		boolean file2_IsFile = file2.isFile();
-		String file2_Name = file2.getName();
-		String file2_AbsPath = file2.getAbsolutePath();
-		String file2_Path = file2.getPath();
-		System.out.println("file2_Src:     '" + file1_AbsPath + "'");
-		System.out.println("file2_IsAbs:   '" + file2_IsAbs + "'");
-		System.out.println("file2_Exists:  '" + file2_Exists + "'");
-		System.out.println("file2_IsDir:   '" + file2_IsDir + "'");
-		System.out.println("file2_IsFile:  '" + file2_IsFile + "'");
-		System.out.println("file2_Name:    '" + file2_Name + "'");
-		System.out.println("file2_Path:    '" + file2_Path + "'");
-		System.out.println("file2_AbsPath: '" + file2_AbsPath + "'");
+		String srcPath2 = file1.getAbsolutePath();
+		File file2 = new File(srcPath2);
+		System.out.println("srcPath2:      '" + srcPath2 + "'");
+		printFileProperties(file2);
 
 		// file 3
 		System.out.println("--- file3 ---");
-		String relPath3 = ".";
-		File file3 = new File(relPath3);
-		boolean file3_IsAbs = file3.isAbsolute();
-		boolean file3_Exists = file3.exists();
-		boolean file3_IsDir = file3.isDirectory();
-		boolean file3_IsFile = file3.isFile();
-		String file3_Name = file3.getName();
-		String file3_AbsPath = file3.getAbsolutePath();
-		String file3_Path = file3.getPath();
-		System.out.println("file3_Src:     '" + relPath3 + "'");
-		System.out.println("file3_IsAbs:   '" + file3_IsAbs + "'");
-		System.out.println("file3_Exists:  '" + file3_Exists + "'");
-		System.out.println("file3_IsDir:   '" + file3_IsDir + "'");
-		System.out.println("file3_IsFile:  '" + file3_IsFile + "'");
-		System.out.println("file3_Name:    '" + file3_Name + "'");
-		System.out.println("file3_Path:    '" + file3_Path + "'");
-		System.out.println("file3_AbsPath: '" + file3_AbsPath + "'");
+		String srcPath3 = ".";
+		File file3 = new File(srcPath3);
+		System.out.println("srcPath3:      '" + srcPath3 + "'");
+		printFileProperties(file3);
+
+		// file 4
+		System.out.println("--- file4 ---");
+		String srcPath4 = "./";
+		File file4 = new File(srcPath4);
+		System.out.println("srcPath4:      '" + srcPath4 + "'");
+		printFileProperties(file4);
+
+		// file 5
+		System.out.println("--- file5 ---");
+		String srcPath5 = "/";
+		File file5 = new File(srcPath5);
+		System.out.println("srcPath5:      '" + srcPath5 + "'");
+		printFileProperties(file5);
+
+		return;
+	}
+
+
+	private static void printFileProperties (File f) {
+		boolean file_IsAbs = f.isAbsolute();
+		boolean file_Exists = f.exists();
+		boolean file_IsDir = f.isDirectory();
+		boolean file_IsFile = f.isFile();
+		String file_Name = f.getName();
+		String file_AbsPath = f.getAbsolutePath();
+		String file_Path = f.getPath();
+		System.out.println("file_toString: '" + f.toString() + "'");
+		System.out.println("file_IsAbs:    '" + file_IsAbs + "'");
+		System.out.println("file_Exists:   '" + file_Exists + "'");
+		System.out.println("file_IsDir:    '" + file_IsDir + "'");
+		System.out.println("file_IsFile:   '" + file_IsFile + "'");
+		System.out.println("file_Name:     '" + file_Name + "'");
+		System.out.println("file_Path:     '" + file_Path + "'");
+		System.out.println("file_AbsPath:  '" + file_AbsPath + "'");
 	}
 
 }
